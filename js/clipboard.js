0d50d51862224ddf4f23e7a8875f989851e3e5b2
Camilo Sasuke Tsumanuma <camilo@ca2.email>
2020-09-04 20:46:25 -0300

    <3tbs, Mummi and bilbo!!
    DonHo raiding contribution!!
    
    2hstrike and elpapara chatting contribution!!
    
    parrot_rl and vue_equalizer (fpgaguru) chatting contribution!!
    
    from bilbo machine

---
+++ js/clipboard.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+
+function copy_to_clipboard(field) {
+  /* Get the text field */
+  var copyText = field
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
